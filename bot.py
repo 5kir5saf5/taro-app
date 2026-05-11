87dc309967829a6c3985ce79602a17f1fbbf96c7
5kir5saf5 <65107697+5kir5saf5@users.noreply.github.com>
2026-05-11 23:21:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,70 +1,43 @@
-
 import asyncio
-import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 
-from config import BOT_TOKEN
-from tarot import draw_cards
-from ai import interpret
-from database import init, can_use, add_use
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+from tarot import draw_card
+
+BOT_TOKEN = "ТВОЙ_ТОКЕН"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def start(msg: types.Message):
-    await msg.answer("🔮 Бот Таро запущен. Напиши свой вопрос.")
+@dp.message(CommandStart())
+async def start(message: types.Message):
 
+    await message.answer(
+        "🔮 Напиши вопрос для гадания"
+    )
 
 
 @dp.message()
-async def handle(msg: types.Message):
+async def reading(message: types.Message):
 
-    print("MESSAGE RECEIVED:", msg.text)
+    card, status, meaning = draw_card()
 
-    user_id = msg.from_user.id
+    text = f"""
+🃏 Карта: {card}
+📌 Положение: {status}
 
-    if not await can_use(user_id):
-        await msg.answer("❌ Лимит на сегодня исчерпан")
-        return
+✨ Значение:
+{meaning}
+"""
 
-    cards = draw_cards()
+    await message.answer(text)
 
-    text_cards = "\n".join([c["name"] for c in cards])
-
-    await msg.answer("🔮 Расклад готов:\n\n" + text_cards)
-
-    # ❌ УБРАЛИ ФОТО
-
-    # AI трактовка
-    result = await asyncio.to_thread(interpret, msg.text, text_cards)
-
-    await msg.answer("🧠 Трактовка:\n\n" + result)
-
-    await add_use(user_id)
 
 async def main():
-    await init()
-    print("BOT STARTED")
-
-    while True:
-        try:
-            await dp.start_polling(
-                bot,
-                timeout=60
-            )
-
-        except asyncio.CancelledError:
-            break
-
-        except Exception as e:
-            print("🌐 CONNECTION ERROR:", e)
-            print("🔄 retry in 5 seconds...")
-            await asyncio.sleep(5)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
